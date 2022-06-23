acbabd5df4ec78c8b8f84a43434d770285418b00
zainabolaniyan <zainabolaniyan95@gmail.com>
2022-06-23 13:29:40 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const myDetails = {
+    name: 'Olaniyan Zainab',
+    height: '1.65m',
+    country: 'Nigeria',
+}
+
+
+console.log(myDetails);
+
+alert(
+    'my name is Zainab Olaniyan, my height is 1.65m, my home country is Nigeria'
+);
